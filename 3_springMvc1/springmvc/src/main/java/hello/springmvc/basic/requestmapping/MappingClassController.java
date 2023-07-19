b86b00c80eb0d2fe80552662f3e94459b90dec3b
[roozia] <[noonsollin@gmail.com]>
2023-07-19 23:52:15 +0900

    2023.07.19 - 여러 유형의 요청 Mapping

---
+++ 3_springMvc1/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,12 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class MappingClassController {
+
+
+}
